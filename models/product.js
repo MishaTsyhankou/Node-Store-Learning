c60751a46c8e2342f3aab9abae92b80c08bf1c98
mishatsyhankou <Mikhail_Tsyhankou@epam.com>
2023-07-27 15:55:58 +0400

    Edit/Delete/Update admin products

--- models/product.js
+++ models/product.js
@@ -2,21 +2,40 @@ const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb;
 
 class Product{
-  constructor(tittle, price, description, imageURL) {
-    this.tittle = tittle;
+  constructor(title, price, description, imageURL, id) {
+    this.title = title;
     this.price = price;
     this.description = description;
     this.imageURL = imageURL;
+    this._id = id ?  new mongodb.ObjectId(id) : null ;
   }
 
   save(){
-    const db = getDb();
-    return db.collection('products').insertOne(this).then(result=>{
+      const db = getDb();
+      let dbOp;
+      if(this._id){
+          // Update product
+          dbOp = db.collection('products').updateOne({_id: this._id}, {$set: this});
+      }else{
+          dbOp.collection('products').insertOne(this)
+      }
+    return dbOp.then(result=>{
       console.log(result)
     }).catch(err=>{
       console.log(err)
     })
 
+  }
+
+  static deleteById(prodId){
+      const db = getDb()
+      return db.collection('products').deleteOne({_id:  new mongodb.ObjectId(prodId)})
+          .then(result =>{
+              console.log('Deleted')
+          })
+          .catch(err=>{
+              console.log(err)
+          })
   }
   static fetchAll () {
     const db = getDb();
@@ -47,6 +66,7 @@ class Product{
           })
   }
 }
+
 
 // const Product = sequelize.define('product', {
 //   id: {
